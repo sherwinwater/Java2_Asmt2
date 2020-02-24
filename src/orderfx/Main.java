633665cf57b3211fb934a9027cb4bc3e4a7770e9
sherwinwater <sherwin.water@gmail.com>
2020-02-24 13:35:20 -0500

    v1

--- src/orderfx/Main.java
+++ src/orderfx/Main.java
@@ -24,44 +24,112 @@ public class Main extends Application {
     private Label lblFirstName = new Label("FirstName");
     private Label lblLastName = new Label("LastName");
     private Label lblCity = new Label("City");
+    private Label lblMsg = new Label("Please input the number(int) and click the image to order");
+
     private TextField txtOrderID = new TextField();
     private TextField txtFirstName = new TextField();
     private TextField txtLastName = new TextField();
     private TextField txtCity = new TextField();
 
-    private Label lblOrderNumberA = new Label("OrderNumberA");
-    private Label lblOrderNumberB = new Label("OrderNumberB");
-    private Label lblOrderNumberC = new Label("OrderNumberC");
-    private Label lblOrderNumberD = new Label("OrderNumberD");
+    private Label lblOrderNumberA = new Label("Quantity of A");
+    private Label lblOrderNumberB = new Label("Quantity of B");
+    private Label lblOrderNumberC = new Label("Quantity of C");
+    private Label lblOrderNumberD = new Label("Quantity of D");
     private TextField txtOrderNumberA = new TextField("0");
     private TextField txtOrderNumberB = new TextField("0");
     private TextField txtOrderNumberC = new TextField("0");
     private TextField txtOrderNumberD = new TextField("0");
 
-    private Image imgA = new Image("images/apple.png");
+    // the following fruit images are from the website:
+    // https://7esl.com/fruits-vocabulary-english/
+    private Image imgA = new Image("images/apple.png", 100, 100, true, true);
     private ImageView imgAview = new ImageView(imgA);
-    private Image imgB = new Image("images/orange.png");
-    private ImageView imgBview = new ImageView(imgB);
-    private Image imgC = new Image("images/strawberry.png");
-    private ImageView imgCview = new ImageView(imgC);
-    private Image imgD = new Image("images/pear.png");
-    private ImageView imgDview = new ImageView(imgD);
+    private Button btnImgA = new Button("", imgAview);
 
-    private Button btnDisplay = new Button("Display");
+    private Image imgB = new Image("images/orange.png", 100, 100, true, true);
+    private ImageView imgBview = new ImageView(imgB);
+    private Button btnImgB = new Button("", imgBview);
+
+    private Image imgC = new Image("images/strawberry.png", 100, 100, true, true);
+    private ImageView imgCview = new ImageView(imgC);
+    private Button btnImgC = new Button("", imgCview);
+
+    private Image imgD = new Image("images/pear.png", 100, 100, true, true);
+    private ImageView imgDview = new ImageView(imgD);
+    private Button btnImgD = new Button("", imgDview);
+
+    private Button btnDisplay = new Button("Display the order details");
+    private int orderNumberA;
+    private int orderNumberB;
+    private int orderNumberC;
+    private int orderNumberD;
+
+    private Order one = new Order();
 
     @Override
     public void start(Stage primaryStage) {
+        orderA(btnImgA, txtOrderNumberA);
+        orderB(btnImgB, txtOrderNumberB);
+        orderC(btnImgC, txtOrderNumberC);
+        orderD(btnImgD, txtOrderNumberD);
+        displayOrder(one);
 
+        Scene scene = new Scene(getGrid(), 500, 300);
+        primaryStage.setTitle("Order The Fruits");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void displayOrder(Order one) {
         btnDisplay.setOnAction((e) -> {
-            Order one = new Order();
             process(one);
             DisplayOrder ds = new DisplayOrder(one);
             ds.show();
         });
-        Scene scene = new Scene(getGrid(), 650, 300);
-        primaryStage.setTitle("Order The Fruits");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    }
+
+    public void orderA(Button btnImg, TextField txtOrderNumber) {
+        btnImg.setOnAction((e) -> {
+            try {
+                orderNumberA = Integer.parseInt(txtOrderNumber.getText());
+            } catch (Exception e1) {
+                txtOrderNumber.setText("0");
+                System.out.println(e1.getMessage());
+            }
+        });
+    }
+
+    public void orderB(Button btnImg, TextField txtOrderNumber) {
+        btnImg.setOnAction((e) -> {
+            try {
+                orderNumberB = Integer.parseInt(txtOrderNumber.getText());
+            } catch (Exception e1) {
+                txtOrderNumber.setText("0");
+                System.out.println(e1.getMessage());
+            }
+        });
+    }
+
+    public void orderC(Button btnImg, TextField txtOrderNumber) {
+        btnImg.setOnAction((e) -> {
+            try {
+                orderNumberC = Integer.parseInt(txtOrderNumber.getText());
+            } catch (Exception e1) {
+                txtOrderNumber.setText("0");
+                System.out.println(e1.getMessage());
+            }
+        });
+    }
+
+    public void orderD(Button btnImg, TextField txtOrderNumber) {
+        btnImg.setOnAction((e) -> {
+            try {
+                orderNumberD = Integer.parseInt(txtOrderNumber.getText());
+            } catch (Exception e1) {
+                txtOrderNumber.setText("0");
+                System.out.println(e1.getMessage());
+            }
+        });
     }
 
     public GridPane getGrid() {
@@ -78,35 +146,32 @@ public class Main extends Application {
         pane.add(txtLastName, 2, 1);
         pane.add(txtCity, 3, 1);
 
-        pane.add(lblOrderNumberA, 0, 2);
-        pane.add(lblOrderNumberB, 1, 2);
-        pane.add(lblOrderNumberC, 2, 2);
-        pane.add(lblOrderNumberD, 3, 2);
+        pane.add(lblMsg, 0, 2, 4, 1);
 
-        pane.add(txtOrderNumberA, 0, 3);
-        pane.add(txtOrderNumberB, 1, 3);
-        pane.add(txtOrderNumberC, 2, 3);
-        pane.add(txtOrderNumberD, 3, 3);
+        pane.add(lblOrderNumberA, 0, 3);
+        pane.add(lblOrderNumberB, 1, 3);
+        pane.add(lblOrderNumberC, 2, 3);
+        pane.add(lblOrderNumberD, 3, 3);
 
-        pane.add(imgAview, 0, 4);
-        pane.add(imgBview, 1, 4);
-        pane.add(imgCview, 2, 4);
-        pane.add(imgDview, 3, 4);
-        pane.add(btnDisplay, 1, 5);
+        pane.add(txtOrderNumberA, 0, 4);
+        pane.add(txtOrderNumberB, 1, 4);
+        pane.add(txtOrderNumberC, 2, 4);
+        pane.add(txtOrderNumberD, 3, 4);
+
+        pane.add(btnImgA, 0, 5);
+        pane.add(btnImgB, 1, 5);
+        pane.add(btnImgC, 2, 5);
+        pane.add(btnImgD, 3, 5);
+        pane.add(btnDisplay, 1, 6, 2, 1);
 
         return pane;
     }
 
     private void process(Order one) {
-        try {
-            one.setOrderNumberA(Integer.parseInt(txtOrderNumberA.getText()));
-            one.setOrderNumberB(Integer.parseInt(txtOrderNumberB.getText()));
-            one.setOrderNumberC(Integer.parseInt(txtOrderNumberC.getText()));
-            one.setOrderNumberD(Integer.parseInt(txtOrderNumberD.getText()));
-        } catch (NumberFormatException e) {
-            System.out.println("OrderNumber must be an integer");
-        }
-
+        one.setOrderNumberA(orderNumberA);
+        one.setOrderNumberB(orderNumberB);
+        one.setOrderNumberC(orderNumberC);
+        one.setOrderNumberD(orderNumberD);
         one.setOrderID(txtOrderID.getText());
         one.setFirstName(txtFirstName.getText());
         one.setLastName(txtLastName.getText());
